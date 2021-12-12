c2b2eba95b187716b50dfbe86743508b45af1a39
yomomma <roboo.jack@gmail.com>
2021-12-12 11:28:39 -0500

    just blink to start

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #just a little script to drive the front lights on a timer with the RaspPi Zero W
 
 import RPi.GPIO as io
 import time
 import datetime
+from pytz import timezone
+
 
 io.setmode(io.BCM)
 led1 = 27
@@ -13,7 +15,8 @@ io.setup(led1, io.OUT)
 while 1:
     # TODO: hook up a photo resistor in place of this
     # 1pm-5pm turn on
-    if datetime.datetime.now().hour >= 12+5 or datetime.datetime.now().hour <= 12+1:
+    now = datetime.datetime.now(timezone("US/Eastern"))
+    if now.hour >= 12+5 or now.hour <= 12+1:
         io.output(led1, True)
     else:
         io.output(led1, False)
