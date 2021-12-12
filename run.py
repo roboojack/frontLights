0511ce9f66315040bbcea4d6789209be4a693737
yomomma <roboo.jack@gmail.com>
2021-12-12 10:56:25 -0500

    just blink to start

--- run.py
+++ run.py
@@ -4,13 +4,18 @@
 
 import RPi.GPIO as io
 import time
+import datetime
 
 io.setmode(io.BCM)
 led1 = 27
 io.setup(led1, io.OUT)
 
 while 1:
-    io.output(led1, True)
-    time.sleep(1)
-    io.output(led1, False)
+    # TODO: hook up a photo resistor in place of this
+    # 1pm-5pm turn on
+    if datetime.datetime.now().hour >= 12+5 or datetime.datetime.now().hour <= 12+1:
+        io.output(led1, True)
+    else:
+        io.output(led1, False)
+
     time.sleep(1)
